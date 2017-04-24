e671cd20dabca0f9b7cf5781a2733a8692ce4ab0
Winchester-Yi <619589959@qq.com>
2017-04-24 22:26:57 +0800

    heap sort

--- sort/sort.c
+++ sort/sort.c
@@ -33,7 +33,8 @@ Output:the sorted array
 *************************************************/
 int merge_sort(int num, int *array, int *sortedArray)
 {
-	merge_sort_(0, int num, int *array, int *sortedArray)
+	merge_sort_(0, int num, int *array, int *sortedArray);
+	return 0;
 }
 
 int merge_sort_(int begin, int end, int *array, int *sortedArray)
@@ -41,14 +42,14 @@ int merge_sort_(int begin, int end, int *array, int *sortedArray)
 	if(begin < end)
 	{
 		int middle;
-		middle = ceil(((float)end+begin)/2);
-		merge_sort(begin, middle, array);
-		merge_sort(middle+1, end, array);
+		middle = ceil((float)end+begin)/2;
+		merge_sort_(begin, middle, array, sortedArray);
+		merge_sort_(middle+1, end, array, sortedArray);
 		merge(begin, middle, end, array);
 	}
-
 	return 0;
 }
+
 int merge(int begin, int middle, int end, int *array, int *sortedArray)
 {
 	watch_1 = begin;
@@ -69,6 +70,58 @@ int merge(int begin, int middle, int end, int *array, int *sortedArray)
 			end--;
 			inedx--;
 		}
+	}
+	return 0;
+}
+
+/*************************************************
+Max-Heap
+void heap_sort(int num, int* list, int* result)
+
+Input:the length of the array, the array
+Output:the sorted array
+*************************************************/
+int max_heap(int *tree, int i)
+{
+	int maxIndex;
+	int l = i<<1;
+	int r = (i<<1)+1;
+	int exchange;
+
+	if ( (l <= heapSize) && ((*(tree+l)) > *(tree+i)) )
+		maxIndex = l;
+	else maxIndex = i;
+
+	if ( (r <= heapSize) && ((*(tree+r)) > *(tree+maxIndex)) )
+		maxIndex = r;
+
+	if ( maxIndex != i )
+	{
+		exchange = *(tree+r);
+		*(tree+r) = *(tree+maxIndex);
+		*(tree+maxIndex) = exchange;
+	}
+
+	max_heap(tree, maxIndex);
+
+	return 0;
+}
+
+int build_max_heap(int *array, int size)
+{
+	int i;
+	for ( i=floor(size/2); i>0; i-- )
+		build_max_heap(array, i);
+	return 0;
+}
+
+int heap_sort(int *array, int &size)
+{
+	int i;
+	build_max_heap(array, *size);
+	for ( i=size; i>1; i-- )
+	{
+		*size
 	}
 	return 0;
 }
