6232f3f3d20d8725768cfd709b1d47de26d9d0b5
Winchester-Yi <619589959@qq.com>
2017-05-01 20:56:20 +0800

    nothing

--- sort/main.c
+++ sort/main.c
@@ -5,13 +5,6 @@
 
 int main()
 {
-	int num[MAX_NUM];
-	int result[MAX_NUM];
-	int i=0;
-	printf("Plese enter numbers:\n");
-	do{
-		scanf("%d",&num[i++]);
-	}while(*(num+i-1)!= "\n");
-	insert_sort(i, num, result);
+	
 	return 0;
 }
